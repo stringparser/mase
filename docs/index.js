3f153353211ab2ad79b4414f91f934675e1e3e87
Javier Carrillo <stringparser@gmail.com>
2015-04-27 22:29:26 +0200

    chore: minor changes for documentation

--- docs/index.js
+++ docs/index.js
@@ -1,30 +1,28 @@
 'use strict';
 
-process.chdir('docs');
-
 var fs = require('fs');
 var util = require('./util');
+
+process.chdir('docs');
 
 var docs = util.getBlocks();
 var summary = fs.createWriteStream('SUMMARY.md');
 
-docs = docs.map(function(block, index){
+docs.forEach(function(block, index){
   var lines = block.split('\n');
   // strip block comment start and end
   lines.shift(); lines.pop();
   // extract title
   var title = lines[0].replace(/[ ]*[#]+/, '').trim();
+
+  // write table of contents
   if(index){
     summary.write(`* [${title}](${title}.md)\n`);
   } else {
     lines[0] = '## module.exports';
   }
 
-  fs.createWriteStream(title+'.md')
+  // write each block on its file
+  fs.createWriteStream('${title}.md')
     .write(lines.join('\n'));
-
-  return {
-    title: util.map[title.toLowerCase()] || title,
-    content: lines.slice(1).join('\n')
-  };
 });
