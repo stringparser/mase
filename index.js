e6c104fbf55338525afc72861a29d1e171060d9c
Javier Carrillo <stringparser@gmail.com>
2015-04-30 17:07:48 +0200

    docs: typo

--- index.js
+++ index.js
@@ -356,7 +356,7 @@ The `oldDoc` and `newDoc` are passed just before the change is done
 ### spec
 
 ```js
-function changed(string method, object newDoc, object oldDocs)
+function changed(string method, object newDoc, object oldDoc)
 ```
 
 _arguments_
